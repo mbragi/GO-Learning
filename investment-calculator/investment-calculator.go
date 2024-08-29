f8e0afd769daca2b4dd28d686a8e0b1aa561b5b9
mbragi <spontaneousmanji@gmail.com>
2024-08-30 00:24:10 +0100

    chore: Update investment calculator to handle user input and improve code readability

--- investment-calculator/investment-calculator.go
+++ investment-calculator/investment-calculator.go
@@ -9,9 +9,16 @@ func main() {
 	const inflationRate = 1000
 	var investmentAmount float64 = 200
 	expectedReturnValue := 2.2
-	years := 5
+	var years int
 
+	fmt.Print("Enter investment amount: ")
 	fmt.Scan(&investmentAmount)
+
+	fmt.Print("Enter expected return value: ")
+	fmt.Scan(&expectedReturnValue)
+
+	fmt.Print("Enter number of years: ")
+	fmt.Scan(&years)
 
 	futureValue := investmentAmount * math.Pow(1+expectedReturnValue/100, float64(years))
 	futureRealValue := futureValue / math.Pow(1+inflationRate/100, float64(years))
