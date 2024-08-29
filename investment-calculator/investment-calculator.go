caea284580453c270b86d0c46773bb51c8a8aa8d
mbragi <spontaneousmanji@gmail.com>
2024-08-29 22:49:57 +0100

    chore: Update investment calculator to use explicit variable types

--- investment-calculator/investment-calculator.go
+++ investment-calculator/investment-calculator.go
@@ -6,10 +6,10 @@ import (
 )
 
 func main() {
-	var investmentAmount = 200
-	var expectedReturnValue = 2.2
-	var years = 5
+	var investmentAmount float64 = 200
+	var expectedReturnValue float64 = 2.2
+	var years int = 5
 
-	var futureValue = float64(investmentAmount) * math.Pow(1+expectedReturnValue/100, float64(years))
+	var futureValue = investmentAmount * math.Pow(1+expectedReturnValue/100, float64(years))
 	fmt.Println(futureValue)
 }
